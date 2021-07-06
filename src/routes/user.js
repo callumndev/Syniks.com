01f4f0ebca41380f313c468c5ea06d37df01c32d
Callum <callumnewsome100@gmail.com>
2021-07-06 01:59:40 +0100

    💄 Add profile page

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,12 @@
 const user = syniks.router();
 
-user.get( '/profile', syniks.util.auth.check, ( req, res ) => syniks.util.render( req, res, 'profile' ) );
+user.get( '/profile', syniks.util.auth.check, ( req, res ) => {
+    let { Permissions } = require( 'discord.js' ),
+    moment = require( 'moment' );
+
+    syniks.util.render( req, res, 'profile', { permissions: Permissions, moment } );
+} );
+
 
 module.exports = user;
 module.exports.config = {
