fd4e98a848efa351fad1d8304a49be840eb6e9c3
Callum <callumnewsome100@gmail.com>
2021-07-07 00:57:17 +0100

    🚸/invited route
    Made for redirects after the bot is invited

--- src/routes/main.js
+++ src/routes/main.js
@@ -4,6 +4,8 @@ main.get( '/', ( req, res ) => syniks.util.render( req, res, 'index' ) );
 
 main.get( '/invite', ( req, res ) => res.redirect( syniks.settings.botInvite ) );
 
+main.get( '/invited', ( req, res ) => res.redirect( req.query.redirect || '/' ) );
+
 main.get( '/support', ( req, res ) => res.redirect( syniks.settings.supportServer ) );
 
 
