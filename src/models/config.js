69f81d78306008365f63c2ababf5ba58d70a1311
Callum <callumnewsome100@gmail.com>
2021-08-03 03:29:10 +0100

    🗃️ Update config model for number guesser

--- src/models/config.js
+++ src/models/config.js
@@ -28,5 +28,11 @@ module.exports = syniks.db.define( 'config', {
     serverStatsEnabled:  { type: syniks.db.DataTypes.BOOLEAN, allowNull: false, defaultValue: syniks.settings.modelDefaults.config.serverStatsEnabled },
 
     // Levelling
-    levelMessageChannel: syniks.db.DataTypes.STRING
+    levelMessageChannel: syniks.db.DataTypes.STRING,
+
+    // Number Guesser
+    numberGuesserChannel: syniks.db.DataTypes.STRING,
+    numberGuesserNumber: syniks.db.DataTypes.INTEGER,
+    numberGuesserNumberMin: syniks.db.DataTypes.INTEGER,
+    numberGuesserNumberMax: syniks.db.DataTypes.INTEGER
 } );
